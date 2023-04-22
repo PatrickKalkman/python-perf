85653c54450778215fec30b696d958cdbfef7471
Patrick Kalkman <patrick@simpletechture.nl>
2023-04-22 11:47:25 +0200

    Fix flake error

--- measuring/time_function.py
+++ measuring/time_function.py
@@ -1,11 +1,13 @@
 import hashlib
 import time
 
+
 def chain_sha1_hash(input_str, iterations):
     current_hash = input_str
     for _ in range(iterations):
         current_hash = hashlib.sha1(current_hash.encode()).hexdigest()
     return current_hash
+
 
 if __name__ == "__main__":
     input_str = "Turbocharge Your Python Code"
